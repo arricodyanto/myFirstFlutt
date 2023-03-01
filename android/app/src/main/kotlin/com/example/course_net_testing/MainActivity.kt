65807931799d7dec2a730be9d6bc8ca173cac035
arricodyanto <arricohandyanto@gmail.com>
2023-03-01 23:17:40 +0700

    first flutter commit

---
+++ android/app/src/main/kotlin/com/example/course_net_testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.course_net_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secondPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Page Kedua'),
+        ),
+        body: const Center(
+          child: Text('Ini adalah body page ke 2'),
+        ));
+  }
+}
